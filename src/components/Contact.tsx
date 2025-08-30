35436312ee198e836bcd54bcee7f14cb9358c6e6
Maanas Varma S H <sh.maanasvarma@gmail.com>
2025-08-30 13:26:45 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,11 +9,11 @@ function Contact() {
       <h2>Contact Me</h2>
       <p>I'm always open to new opportunities and collaborations. Feel free to reach out!</p>
       <div className="contact-info">
-        <p><FaEnvelope className="contact-icon" /> Email: <a href="mailto:sh.maanasvarma@gmail.com">sh.maanasvarma@gmail.com</a></p>
-        <p><FaPhone className="contact-icon" /> Phone: <a href="tel:+91 6362847181">+91 6362847181</a></p>
-        <p><FaLinkedin className="contact-icon" /> LinkedIn: <a href="https://www.linkedin.com/in/maanas-varma-59429327b/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BNyzzOA35T2mGF0WR%2Bv%2FcgQ%3D%3D" target="_blank" rel="noopener noreferrer">Maanas Varma</a></p>
-        <p><FaGithub className="contact-icon" /> GitHub: <a href="https://github.com/maanasvarma2003" target="_blank" rel="noopener noreferrer">GitHub Profile</a></p>
-        <p><FaInstagram className="contact-icon" /> Instagram: <a href="https://www.instagram.com/maanas_varma/#" target="_blank" rel="noopener noreferrer">maanas_varma</a></p>
+        <p><FaEnvelope className="contact-icon" /> : <a href="mailto:sh.maanasvarma@gmail.com">sh.maanasvarma@gmail.com</a></p>
+        <p><FaPhone className="contact-icon" /> : <a href="tel:+91 6362847181">+91 6362847181</a></p>
+        <p><FaLinkedin className="contact-icon" /> : <a href="https://www.linkedin.com/in/maanas-varma-59429327b/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BNyzzOA35T2mGF0WR%2Bv%2FcgQ%3D%3D" target="_blank" rel="noopener noreferrer">Maanas Varma</a></p>
+        <p><FaGithub className="contact-icon" /> : <a href="https://github.com/maanasvarma2003" target="_blank" rel="noopener noreferrer">GitHub Profile</a></p>
+        <p><FaInstagram className="contact-icon" /> : <a href="https://www.instagram.com/maanas_varma/#" target="_blank" rel="noopener noreferrer">maanas_varma</a></p>
       </div>
     </section>
   );
